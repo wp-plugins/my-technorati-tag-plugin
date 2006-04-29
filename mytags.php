2ed4d8fdce0400c9b1adf8f6b966bfa88309cc8f
switchhook <switchhook@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2006-04-29 17:37:17 +0000

    Convered this over to useing wp-cache so that it will Use the WP 2.0 cache
    git-svn-id: https://plugins.svn.wordpress.org/my-technorati-tag-plugin/trunk@5964 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- mytags.php
+++ mytags.php
@@ -4,7 +4,7 @@ Plugin Name: My Technorati Tag Cloud
 Plugin URI: http://blog.mericson.com/
 Description: This will download your technorati tags and display them as a tag cloud 
 Author: Matt Ericson
-Version: 0.01
+Version: 1.00
 Author URI: http://blog.mericson.com/
 
 INSTRUCTIONS
@@ -19,38 +19,24 @@ You may also request the number of tags you want to see the default is 20.
 
 <?php  technoratiTagCloud("url","api key", "num of tag"); ?>
 
-*/
-//include_once("wp-includes/rss-functions.php");
+This version of the code will use WP 2.0 caching to use it correctly you 
+need to create a "wp-content/cache" directory that is writeable by the 
+Web user
 
-function technoratiTagCloud($url , $apiKey, $limit=20, $cache_time = 600, $cache_file = null) {
+*/
+
+function technoratiTagCloud($url , $apiKey, $limit=20, $cache_time = 600) {
 
 
     $url = urlencode($url);
     $api_url = "http://api.technorati.com/blogposttags?url=" . $url . "&key=" . $apiKey . "&limit=" . $limit;
-    if ($cache_file == null) {
-        $cache_file  =  "/tmp/techtagcloud.$url.$cache_time.$limit.cache";
-    }
 
-    echo "<style type='text/css'>
-.heatmap {font-size: 0.97em;}
-.heatmap li {display: inline;}
-.heatmap em {font-style: normal; font-size: 1.03em;}
-</style>
-";
+    $cacheKey = "techtagcloud.$url.$limit";
 
+    wp_cache_init();
+    $data = wp_cache_get($cacheKey);
 
-    $cache_file_tmp = "$cache_file.tmp";
-
-    $time = split(" ", microtime());
-    srand((double)microtime()*1000000);
-
-    $cache_time_rnd = 30 - rand(0, 60);
-    if (
-    !file_exists($cache_file)
-    || !filesize($cache_file) > 20
-    || ((filemtime($cache_file) + $cache_time - $time[1]) + $cache_time_rnd < 0)
-    || (filemtime(__FILE__) > filemtime($cache_file))
-    ) {
+    if (! $data) {
         $c = curl_init($api_url);
         curl_setopt($c, CURLOPT_RETURNTRANSFER,1);
         curl_setopt($c, CURLOPT_CONNECTTIMEOUT, 2);
@@ -62,28 +48,22 @@ function technoratiTagCloud($url , $apiKey, $limit=20, $cache_time = 600, $cache
         $curl_error_code = $info['http_code'];
         curl_close($c);
         if ($curl_error_code == 200) {
-            $fpwrite = fopen($cache_file_tmp, 'w');
-            if ($fpwrite){
-                fputs($fpwrite, $response);
-                fclose($fpwrite);
-                rename($cache_file_tmp, $cache_file);
-            }
-        }
-        if ((file_exists($cache_file)) && filesize($cache_file) > 20)  {
             $data = $response;
 
+            wp_cache_set($cacheKey,$data,'',$cache_time);
+            wp_cache_close();
         } elseif ($curl_error_code) {
             //do something here
         } else {
             //do something here
         }
-    } else {
-        $data = file_get_contents($cache_file);
     }
     //Now I parse and show the responce
 
+    
     $xml = XML_unserialize($data);
 
+    
     $items = $xml['tapi']['document']['item'];
     if (!isset($items[0])) {
         $tmp = $items;
@@ -111,6 +91,13 @@ function technoratiTagCloud($url , $apiKey, $limit=20, $cache_time = 600, $cache
 
     if ($max==$min) $scale=1.0;
     $scale = $scale / ($rangemax-$rangemin);
+
+    echo "<style type='text/css'>
+.heatmap {font-size: 0.97em;}
+.heatmap li {display: inline;}
+.heatmap em {font-style: normal; font-size: 1.03em;}
+</style>
+";
 
     if (sizeof($tagData)) {
         ksort($tagData);
